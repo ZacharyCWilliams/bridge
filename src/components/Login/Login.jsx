a4f31c3e714922f7d473bb297103bc325fea2642
zach <zachcwilliams@icloud.com>
2021-02-05 15:15:58 -0800

    completed login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -36,28 +36,52 @@ const Login = () => {
   }
 
   return (
-    <div className={styles.loginWrapper}>
-      <Link className={styles.loginExit} to="/">
-        <ClearTwoToneIcon />
-      </Link>
+    <div className={styles.modalBackground}>
       <div className={styles.loginContainer}>
-        <div>
-          <img className={styles.moneyImage} src={"https://cdn1.vectorstock.com/i/1000x1000/01/50/money-loss-down-arrow-stocks-graph-concept-of-vector-25000150.jpg"} alt=""/>
+        <div className={styles.exitBtn}>
+          <Link className={styles.exitBtnLink} to="/">
+            <ClearTwoToneIcon />
+          </Link>
         </div>
-        <div>
-          <h1 className={styles.signInHeader}>Sign in</h1>
-          <p className={styles.signInParagraph}>Walk the Bridge to financial freedom</p>
-          <form className={styles.formWrapper} onSubmit={e => login(e)}>
-            <label className={styles.loginEmailSvg}>
-              <DraftsTwoToneIcon />
-            </label>
-            <input className={styles.loginInput} onChange={e => setEmail(e.target.value)} type="text" placeholder="Email" />
-            <label className={styles.loginPasswordSvg}>
-              <LockTwoToneIcon />
-            </label>
-            <input className={styles.loginInput} onChange={e => setPassword(e.target.value)} type="password" placeholder="Password" />
+
+
+        <div className={styles.loginImage}>
+          <img src={"https://designcode.io/images/illustrations/teamwork.svg"} alt=""/>
+        </div>
+
+        <div className={styles.formWrapper}>
+          <h1 className={styles.header}>Sign in</h1>
+          <p className={styles.tagline}>Welcome back! Let's get to work.</p>
+          
+          <form className={styles.loginWrapper} onSubmit={e => login(e)}>
+            <div className={styles.inputWrapper}>
+              <label className={styles.formInputLabel}>
+                <DraftsTwoToneIcon />
+              </label>
+              <input className={styles.loginInput} onChange={e => setEmail(e.target.value)} type="text" placeholder="Email" />
+            </div>
+            <div className={styles.inputWrapper}>
+              <label className={styles.formInputLabel}>
+                <LockTwoToneIcon />
+              </label>
+              <input className={styles.loginInput} onChange={e => setPassword(e.target.value)} type="password" placeholder="Password" />
+            </div>
+
             <button className={styles.loginBtn}>SIGN IN</button>
           </form>
+          
+          <div className={styles.footerWrapper}>
+            <div className={styles.flex}>
+              <p className={styles.linkToRegisterParagraph}>Need an account?</p>
+              <Link className={styles.linkToRegister} to="/register">Sign Up</Link>
+            </div>
+            <div className={styles.flex}>
+              <p className={styles.linkToRegisterParagraph}>Forgot your password?</p>
+              <Link className={styles.linkToRegister} to="/recover">Reset Password</Link>
+            </div>
+           
+          </div>
+
         </div>
       </div>
     </div>
@@ -65,3 +89,59 @@ const Login = () => {
 }
 
 export default Login;
+
+{/* <div className={styles.modalBackground}>
+      <div className={styles.signUpContainer}>
+        <div className={styles.exitBtn}>
+          <Link className={styles.exitBtnLink} to="/">
+            <ClearTwoToneIcon />
+          </Link>
+        </div>
+
+
+        <div className={styles.registerImage}>
+          <img src={"https://designcode.io/images/illustrations/teamwork.svg"} alt=""/>
+        </div>
+
+
+        <div className={styles.formWrapper}>
+          <h1 className={styles.header}>
+            Sign Up
+          </h1>
+
+          <p className={styles.tagline}>
+            { "The Bridge provides a step by step guide to financial freedom. We invite you to take your first step today." }  
+          </p>
+
+          <form className={styles.registerForm} onSubmit={registerUser}>
+            <div className={styles.inputWrapper}>
+
+              <label className={styles.formInputLabel}>
+                <PersonOutlineTwoToneIcon />
+              </label>
+              <input className={styles.registerInput} onChange={e => setName(e.target.value)} type="text" placeholder="Full Name" />
+            </div>
+            <div className={styles.inputWrapper}>
+              <label className={styles.formInputLabel}>
+                <DraftsTwoToneIcon />
+              </label>
+              <input className={styles.registerInput} onChange={e => setEmail(e.target.value)} type="text" placeholder="Email" />
+            </div>
+            <div className={styles.inputWrapper}>
+              <label className={styles.formInputLabel}>
+                <LockTwoToneIcon />
+              </label>
+              <input className={styles.registerInput} onChange={e => setPassword(e.target.value)} type="password" placeholder="Password" />
+            </div>
+            <button className={styles.registerBtn}>Sign Up</button>
+          </form>
+
+
+            <p className={styles.disclaimer}>By clicking on Sign up, you agree to our Terms of service and Privacy policy.</p>
+            <div className={styles.footerWrapper}>
+              <p className={styles.linkToLoginParagraph}>Already have an account? </p>
+              <Link className={styles.linkToLogin} to="/login">Sign In</Link>
+            </div>
+        </div>
+      </div>
+    </div> */}
